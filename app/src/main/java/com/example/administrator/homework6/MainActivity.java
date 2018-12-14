1b190ecb978cce50746b86a6706a0920148f5b4b
KawhiYy <45116594+KawhiYy@users.noreply.github.com>
2018-12-14 11:37:34 +0800

    Update MainActivity.java

--- app/src/main/java/com/example/administrator/homework6/MainActivity.java
+++ app/src/main/java/com/example/administrator/homework6/MainActivity.java
@@ -13,6 +13,7 @@ import java.util.List;
 public class MainActivity extends AppCompatActivity {
     ViewPager viewPager;
     TabLayout tb;
+    static int position=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,35 +26,42 @@ public class MainActivity extends AppCompatActivity {
 
         List<Fragment> fragments = new ArrayList<>();
         List<Bundle> list = new ArrayList<>();
-        Bundle bundle = new Bundle();
-        bundle.putInt("fruit",R.drawable.apple);
-        bundle.putString("sg","我是苹果");
-        list.add(bundle);
+       for(int j=0;j<8;j++){
+           Bundle bundle = new Bundle();
+           bundle.putInt("fruit",R.drawable.apple);
+           bundle.putString("sg","我是苹果");
+           bundle.putInt("position",j*4);
+           list.add(bundle);
 
-        Bundle bundle1 = new Bundle();
-        bundle1.putInt("fruit",R.drawable.orange);
-        bundle1.putString("sg","我是橘子");
-        list.add(bundle1);
+           Bundle bundle1 = new Bundle();
+           bundle1.putInt("fruit",R.drawable.orange);
+           bundle1.putString("sg","我是橘子");
+           bundle1.putInt("position",j*4+1);
+           list.add(bundle1);
 
-        Bundle bundle2 = new Bundle();
-        bundle2.putInt("fruit",R.drawable.banana);
-        bundle2.putString("sg","我是香蕉");
-        list.add(bundle2);
+           Bundle bundle2 = new Bundle();
+           bundle2.putInt("fruit",R.drawable.banana);
+           bundle2.putString("sg","我是香蕉");
+           bundle2.putInt("position",j*4+2);
+           list.add(bundle2);
 
-        Bundle bundle3 = new Bundle();
-        bundle3.putInt("fruit",R.drawable.pear);
-        bundle3.putString("sg","我是梨子");
-        list.add(bundle3);
+           Bundle bundle3 = new Bundle();
+           bundle3.putInt("fruit",R.drawable.pear);
+           bundle3.putString("sg","我是梨子");
+           bundle3.putInt("position",j*4+3);
+           list.add(bundle3);
+       }
         String Title[] = new String[30];
         for(int i = 0;i<30;i++){
             Title[i] = "TAB" + (i + 1);
             MyFragment mf = new MyFragment();
-            Bundle bd = list.get(i%4);
+            Bundle bd = list.get(i);
+
             mf.setArguments(bd);
             fragments.add(mf);
         }
 
-        
+
         MyFragmentPagerAdapter mfpa = new MyFragmentPagerAdapter(getSupportFragmentManager()
         ,fragments,Title);
         viewPager.setAdapter(mfpa);
